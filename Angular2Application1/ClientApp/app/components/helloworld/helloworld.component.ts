61d964d9e67f625c5f4342c8b6a77e17249058fd
Saad-Ali-Shaikh <saadalishaikh@yahoo.com>
2017-04-17 16:15:06 +0500

    ASP .Net Core WEB API & Angular 2
    This is simple weather app, created after following tutorial on a web
    site. It connects with Openweathermap api and brings temperature of a
    country.

---
+++ Angular2Application1/ClientApp/app/components/helloworld/helloworld.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'hello-world',
+    template: require('./helloworld.component.html')
+})
+export class HelloWorldComponent {
+
+}
---
+++ Angular2Application1/Controllers/WeatherController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Angular2Application1.Controllers
+{
+    [Route("api/[controller]")]
+    public class WeatherController : Controller
+    {
+        // GET: api/values
+        [HttpGet("[action]/{city}")]
+        public async Task<IActionResult> City(string city)
+        {
+            //return Ok(new { Temp = "33", Summary = "it good weather", city = "Lahore" });
+            using (var httpClient = new HttpClient())
+            {
+                try
+                {
+                    httpClient.BaseAddress = new Uri("http://api.openweathermap.org");
+                    var response = await httpClient.GetAsync(string.Format(@"/data/2.5/weather?q={0}&appid=f28302bf2d414b61f7cfd455270c0cdf&units=metric", city));
+                    response.EnsureSuccessStatusCode();
+
+                    var stringResult = await response.Content.ReadAsStringAsync();
+                    var rawWeather = JsonConvert.DeserializeObject<OpenWeatherResponse>(stringResult);
+                    return Ok(new
+                    {
+                        Temp = rawWeather.Main.Temp,
+                        Summary = string.Join(",", rawWeather.Weather.Select(x => x.Main)),
+                        City = rawWeather.Name
+                    });
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest($"Error getting weather from OpenWeather: {ex.Message}");
+                }
+            }
+        }
+
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+    public class OpenWeatherResponse
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<WeatherDescription> Weather { get; set; }
+
+        public Main Main { get; set; }
+    }
+
+    public class WeatherDescription
+    {
+        public string Main { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Main
+    {
+        public string Temp { get; set; }
+    }
+}
